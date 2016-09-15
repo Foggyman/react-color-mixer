be2fe3fc9506c0eb6093a0b81ab8f9b918cac0f6
Foggyman <nicola.crovetti@wind.it>
2016-09-15 10:59:43 +0200

    Projects setting

---
+++ main.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
